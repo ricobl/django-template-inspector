4d4796d5cd9b2e508a5e882b3bd032265ad50d3a
Claudio <jose.claudio@corp.globo.com>
2010-08-09 11:56:54 -0300

    First passing test. Uhay!

---
+++ tip/management/commands/tip.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    help = u"""
+Show information about
+
+"""
+    verbose = True
---
+++ tip/management/tests/functional/test_basic_command.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+def test_basic_template_listing():
+    """basic tests for creating command infrastucture"""
+    
+    assert i_have_a_tip_module_in_management()
+    assert i_have_an_instance_of_tip_command()
+
+
+def i_have_a_tip_module_in_management():
+    from tip.management.commands import tip
+    return tip
+
+def i_have_an_instance_of_tip_command():
+    from tip.management.commands import tip
+    command = tip.Command()
+    return command
