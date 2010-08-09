6a528795655a79967f1d8a085c1c6b7722da5d5d
Claudio <jose.claudio@corp.globo.com>
2010-08-09 10:49:55 -0300

    changing basic structure

---
+++ README.md
@@ -0,0 +1,29 @@
+# Django Template Inspector (tip) 0.0.1
+
+Inspector for Django Templates
+
+## Dependencies
+
+1. TBD
+
+## Installing
+
+1. Install the python module in your system:
+`python setup.py install`
+
+2. Install the app within your django project by editing your `settings.py` file:
+
+    `INSTALLED_APPS = (
+        'django.contrib.auth',
+        'django.contrib.admin',
+        'django.contrib.contenttypes',
+        'django.contrib.sessions',
+        'django.contrib.sites',
+        ...
+        'tip',
+    )`
+
+## Features
+
+1. Template path listing
+    
---
+++ setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# <Django Media Lint - CSS and JS lint checker for django>
+# Copyright (C) <2010>  Jose Claudio Figueiredo <jcfigueiredo@gmail.com>
+# Copyright (C) <2010>  Enrico Batista <ricobl@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from setuptools import setup, find_packages
+from tip import __version__
+
+setup(name='Django Template Isnpector',
+    version=__version__,
+    description='Inspector for Django Templates',
+    author='Enrico Batista',
+    author_email='ricobl@gmail.com',
+    url='http://github.com/ricobl/django-template-inspector',
+    packages=find_packages(),
+    include_package_data = True,
+    package_data = {
+        'tip': [],
+    },
+)
