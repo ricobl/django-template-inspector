0bb478814284b7be1881edf368de40b46d440088
Claudio Figueiredo <claudio@vbox.(none)>
2010-08-09 23:34:25 -0300

    starting template path tests

--- tip/test_template_listing.py
+++ tip/test_template_listing.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
 
-def test_retrieving_path_based_on_setting():
-    i_have_template_dirs_defined_in_settings()
+#def test_retrieving_path_based_on_setting():
+#    i_have_template_dirs_defined_in_settings()
 
 
-def i_have_template_dirs_defined_in_settings():
-    from django.conf import settings
-    assert settings.TEMPLATE_DIRS
+#def i_have_template_dirs_defined_in_settings():
+#    from django.conf import settings
+#    assert settings.TEMPLATE_DIRS
