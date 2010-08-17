880a6742cfa1c11278bf3c03723ee7975158198f
Enrico <rico.bl@gmail.com>
2010-08-17 02:07:04 -0300

    Added test for unknown sub-command.

--- tip/tests/functional/test_basic_infra.py
+++ tip/tests/functional/test_basic_infra.py
@@ -1,17 +1,25 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
 
+from nose.tools import assert_raises
+from django.core.management.base import CommandError
+
 def test_command_infrastructure():
     """basic tests for creating command infrastucture"""
     
     assert i_have_a_tip_module_in_management()
     assert i_have_an_instance_of_tip_command()
 
+    tip_command_rejects_unknown_sub_command()
+
 def i_have_a_tip_module_in_management():
     from tip.management.commands import tip
     return tip
 
 def i_have_an_instance_of_tip_command():
-    from tip.management.commands import tip
-    command = tip.Command()
-    return command
+    from tip.management.commands.tip import Command
+    return Command()
+
+def tip_command_rejects_unknown_sub_command():
+    from tip.management.commands.tip import Command
+    assert_raises(CommandError, Command().handle, 'unknown_sub_command')
